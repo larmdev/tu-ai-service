35e2865bd5624c0cbdfc9e8b2279957b39398297
makremix0510 <makremixx0510@gmail.com>
2026-01-29 14:01:37 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -192,6 +192,7 @@ async def process_single_chunk(chunk_idx, start_page, end_page, pdf_bytes, refId
             "data": data1
         }
 
+        print(f"Callback URL: {CALLBACK_URL}/g{section}")
         # ยิง Callback
         async with httpx.AsyncClient() as client:
             resp = await client.post(f"{CALLBACK_URL}/g{section}", json=payload, timeout=60.0)
