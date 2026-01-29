d372b573b5b63afecde40da8e0a0a6f4b7cb4975
larmdev <78774065+larmdev@users.noreply.github.com>
2026-01-29 17:08:23 +0700

    update main.py

--- src/main.py
+++ src/main.py
@@ -15,7 +15,6 @@ from function.fn_pdf_from_url import load_pdf_from_url
 from function.fn_pdf_text_table import text_with_tables
 
 
-
 load_dotenv()
 
 OPEN_ROUTER_KEY = os.getenv("OPEN_ROUTER_KEY")
@@ -26,161 +25,241 @@ app = FastAPI()
 
 from pydantic import BaseModel
 from typing import Optional
+
+
 class ChunkRequest(BaseModel):
     refId: str
     url: str
     fileName: Optional[str] = None
 
 
-async def process_single_chunk(chunk_idx, start_page, end_page, pdf_bytes, refId, fileName, start_chunk_page):
+async def process_single_chunk(
+    chunk_idx, start_page, end_page, pdf_bytes, refId, fileName, start_chunk_page
+):
     try:
 
-        if chunk_idx == 4 :
+        if chunk_idx == 4:
             return "chunk 4"
         start_page = start_chunk_page[chunk_idx]
         end_page = start_chunk_page[chunk_idx + 1]
-        
-        if start_page is None or end_page is None :
+
+        if start_page is None or end_page is None:
             return "no start/end page"
         chunk_pdf_bytes = slice_pdf_pages(
-            pdf_bytes=pdf_bytes,
-            start_page=start_page,
-            end_page=end_page
+            pdf_bytes=pdf_bytes, start_page=start_page, end_page=end_page
         )
 
         text = None
 
         if chunk_idx == 0 and start_page is not None and end_page is not None:
             from fn_chunk.fn_chunk1 import schema_prompt
-            schema, prompt, master_schema = schema_prompt(chunk_pdf_bytes=chunk_pdf_bytes)
+
+            schema, prompt, master_schema = schema_prompt(
+                chunk_pdf_bytes=chunk_pdf_bytes
+            )
 
         elif chunk_idx == 1 and start_page is not None and end_page is not None:
             from fn_chunk.fn_chunk2 import schema_prompt
-            schema, prompt, master_schema = schema_prompt(chunk_pdf_bytes=chunk_pdf_bytes)
+
+            schema, prompt, master_schema = schema_prompt(
+                chunk_pdf_bytes=chunk_pdf_bytes
+            )
 
         elif chunk_idx == 2 and start_page is not None and end_page is not None:
             from fn_chunk.fn_chunk3 import schema_prompt
-            schema, prompt, master_schema = schema_prompt(chunk_pdf_bytes=chunk_pdf_bytes)
+
+            schema, prompt, master_schema = schema_prompt(
+                chunk_pdf_bytes=chunk_pdf_bytes
+            )
 
         elif chunk_idx == 3 and start_page is not None and end_page is not None:
             from fn_chunk.fn_chunk4_1_1 import schema_prompt
-            schema, prompt, master_schema = schema_prompt(chunk_pdf_bytes=chunk_pdf_bytes)
+
+            schema, prompt, master_schema = schema_prompt(
+                chunk_pdf_bytes=chunk_pdf_bytes
+            )
 
         elif chunk_idx == 5 and start_page is not None and end_page is not None:
             from fn_chunk.fn_chunk5_1_1 import schema_prompt
-            schema, prompt, master_schema = schema_prompt(chunk_pdf_bytes=chunk_pdf_bytes)
+
+            schema, prompt, master_schema = schema_prompt(
+                chunk_pdf_bytes=chunk_pdf_bytes
+            )
 
             # text = text_with_tables(chunk_pdf_bytes)
             # chunk_pdf_bytes = None
 
         elif chunk_idx == 6 and start_page is not None and end_page is not None:
             from fn_chunk.fn_chunk6_1_1 import schema_prompt
-            schema, prompt, master_schema = schema_prompt(chunk_pdf_bytes=chunk_pdf_bytes)
+
+            schema, prompt, master_schema = schema_prompt(
+                chunk_pdf_bytes=chunk_pdf_bytes
+            )
 
         elif chunk_idx == 7 and start_page is not None and end_page is not None:
             from fn_chunk.fn_chunk7 import schema_prompt
-            schema, prompt, master_schema = schema_prompt(chunk_pdf_bytes=chunk_pdf_bytes)
+
+            schema, prompt, master_schema = schema_prompt(
+                chunk_pdf_bytes=chunk_pdf_bytes
+            )
 
         elif chunk_idx == 8 and start_page is not None and end_page is not None:
             from fn_chunk.fn_chunk8 import schema_prompt
-            schema, prompt, master_schema = schema_prompt(chunk_pdf_bytes=chunk_pdf_bytes)
+
+            schema, prompt, master_schema = schema_prompt(
+                chunk_pdf_bytes=chunk_pdf_bytes
+            )
 
         elif chunk_idx == 9 and start_page is not None and end_page is not None:
             from fn_chunk.fn_chunk9 import schema_prompt
-            schema, prompt, master_schema = schema_prompt(chunk_pdf_bytes=chunk_pdf_bytes)
 
-        data1 = call_openrouter_pdf(api_key=OPEN_ROUTER_KEY,model=MODEL,prompt=prompt,schema=schema,pdf_bytes=chunk_pdf_bytes,text = text,
-            engine="pdf-text", #"mistral-ocr" สำหรับรูปภาพ
+            schema, prompt, master_schema = schema_prompt(
+                chunk_pdf_bytes=chunk_pdf_bytes
+            )
+
+        data1 = call_openrouter_pdf(
+            api_key=OPEN_ROUTER_KEY,
+            model=MODEL,
+            prompt=prompt,
+            schema=schema,
+            pdf_bytes=chunk_pdf_bytes,
+            text=text,
+            engine="pdf-text",  # "mistral-ocr" สำหรับรูปภาพ
             temperature=0.00,
         )
 
         if chunk_idx == 0:
             from regex.fn_clean1 import clean
-            data1 = clean (master_schema=master_schema,data1=data1)
+
+            data1 = clean(master_schema=master_schema, data1=data1)
 
         if chunk_idx == 1:
             from regex.fn_clean2 import clean
-            data1 = clean (master_schema=master_schema,data1=data1)
+
+            data1 = clean(master_schema=master_schema, data1=data1)
 
         if chunk_idx == 2:
             from regex.fn_clean3 import clean
-            data1 = clean (master_schema=master_schema,data1=data1)
 
-        if chunk_idx == 3 :
+            data1 = clean(master_schema=master_schema, data1=data1)
+
+        if chunk_idx == 3:
             from fn_chunk.fn_chunk4_1_2 import schema_prompt
+
             schema, prompt = schema_prompt(chunk_pdf_bytes=chunk_pdf_bytes)
-            data2 = call_openrouter_pdf(api_key=OPEN_ROUTER_KEY,model=MODEL,prompt=prompt,schema=schema,pdf_bytes=chunk_pdf_bytes,text = text,
-                engine="pdf-text", #"mistral-ocr" สำหรับรูปภาพ
+            data2 = call_openrouter_pdf(
+                api_key=OPEN_ROUTER_KEY,
+                model=MODEL,
+                prompt=prompt,
+                schema=schema,
+                pdf_bytes=chunk_pdf_bytes,
+                text=text,
+                engine="pdf-text",  # "mistral-ocr" สำหรับรูปภาพ
                 temperature=0.00,
             )
 
             from fn_chunk.fn_chunk4_2 import schema_prompt
+
             schema, prompt = schema_prompt(chunk_pdf_bytes=chunk_pdf_bytes)
 
-            start_page = start_chunk_page[chunk_idx+1]
-            end_page = start_chunk_page[chunk_idx+2]
+            start_page = start_chunk_page[chunk_idx + 1]
+            end_page = start_chunk_page[chunk_idx + 2]
             chunk_pdf_bytes = slice_pdf_pages(
-                pdf_bytes=pdf_bytes,
-                start_page=start_page,
-                end_page=end_page
+                pdf_bytes=pdf_bytes, start_page=start_page, end_page=end_page
             )
 
-            data3 = call_openrouter_pdf(api_key=OPEN_ROUTER_KEY,model=MODEL,prompt=prompt,schema=schema,pdf_bytes=chunk_pdf_bytes,text = text,
-                engine="pdf-text", #"mistral-ocr" สำหรับรูปภาพ
+            data3 = call_openrouter_pdf(
+                api_key=OPEN_ROUTER_KEY,
+                model=MODEL,
+                prompt=prompt,
+                schema=schema,
+                pdf_bytes=chunk_pdf_bytes,
+                text=text,
+                engine="pdf-text",  # "mistral-ocr" สำหรับรูปภาพ
                 temperature=0.00,
             )
 
             from regex.fn_clean4 import clean
-            data1 = clean (master_schema=master_schema,data1=data1,data2=data2,data3=data3)
 
-        if chunk_idx == 5 :
+            data1 = clean(
+                master_schema=master_schema, data1=data1, data2=data2, data3=data3
+            )
+
+        if chunk_idx == 5:
             from fn_chunk.fn_chunk5_1_2 import schema_prompt
+
             schema, prompt = schema_prompt(chunk_pdf_bytes=chunk_pdf_bytes)
             text = text_with_tables(chunk_pdf_bytes)
             chunk_pdf_bytes2 = None
-            data2 = call_openrouter_pdf(api_key=OPEN_ROUTER_KEY,model=MODEL,prompt=prompt,schema=schema,pdf_bytes=chunk_pdf_bytes2,text = text,
-                engine="pdf-text", #"mistral-ocr" สำหรับรูปภาพ
+            data2 = call_openrouter_pdf(
+                api_key=OPEN_ROUTER_KEY,
+                model=MODEL,
+                prompt=prompt,
+                schema=schema,
+                pdf_bytes=chunk_pdf_bytes2,
+                text=text,
+                engine="pdf-text",  # "mistral-ocr" สำหรับรูปภาพ
                 temperature=0.00,
             )
-            text=None
+            text = None
 
             from fn_chunk.fn_chunk5_1_3 import schema_prompt
+
             schema, prompt = schema_prompt(chunk_pdf_bytes=chunk_pdf_bytes)
-            data3 = call_openrouter_pdf(api_key=OPEN_ROUTER_KEY,model=MODEL,prompt=prompt,schema=schema,pdf_bytes=chunk_pdf_bytes,text = text,
-                engine="pdf-text", #"mistral-ocr" สำหรับรูปภาพ
+            data3 = call_openrouter_pdf(
+                api_key=OPEN_ROUTER_KEY,
+                model=MODEL,
+                prompt=prompt,
+                schema=schema,
+                pdf_bytes=chunk_pdf_bytes,
+                text=text,
+                engine="pdf-text",  # "mistral-ocr" สำหรับรูปภาพ
                 temperature=0.00,
             )
 
             from regex.fn_clean5 import clean
-            data1 = clean (master_schema=master_schema,data1=data1,data2=data2,data3=data3)
 
-        if chunk_idx == 6 :
+            data1 = clean(
+                master_schema=master_schema, data1=data1, data2=data2, data3=data3
+            )
+
+        if chunk_idx == 6:
             from fn_chunk.fn_chunk6_1_2 import schema_prompt
+
             schema, prompt = schema_prompt(chunk_pdf_bytes=chunk_pdf_bytes)
-            data2 = call_openrouter_pdf(api_key=OPEN_ROUTER_KEY,model=MODEL,prompt=prompt,schema=schema,pdf_bytes=chunk_pdf_bytes,text = text,
-                engine="pdf-text", #"mistral-ocr" สำหรับรูปภาพ
+            data2 = call_openrouter_pdf(
+                api_key=OPEN_ROUTER_KEY,
+                model=MODEL,
+                prompt=prompt,
+                schema=schema,
+                pdf_bytes=chunk_pdf_bytes,
+                text=text,
+                engine="pdf-text",  # "mistral-ocr" สำหรับรูปภาพ
                 temperature=0.00,
             )
 
             from regex.fn_clean6 import clean
-            data1 = clean (master_schema=master_schema,data1=data1,data2=data2)
+
+            data1 = clean(master_schema=master_schema, data1=data1, data2=data2)
 
         if chunk_idx == 7:
             from regex.fn_clean7 import clean
-            data1 = clean (master_schema=master_schema,data1=data1)
+
+            data1 = clean(master_schema=master_schema, data1=data1)
 
         if chunk_idx == 8:
             from regex.fn_clean8 import clean
-            data1 = clean (master_schema=master_schema,data1=data1)
+
+            data1 = clean(master_schema=master_schema, data1=data1)
 
         if chunk_idx == 9:
             from regex.fn_clean9 import clean
-            data1 = clean (master_schema=master_schema,data1=data1)
 
-        
-        if chunk_idx >= 4 :
+            data1 = clean(master_schema=master_schema, data1=data1)
+
+        if chunk_idx >= 4:
             plusnum = 0
-        else :
+        else:
             plusnum = 1
 
         section = str(chunk_idx + plusnum)
@@ -189,26 +268,33 @@ async def process_single_chunk(chunk_idx, start_page, end_page, pdf_bytes, refId
             "refId": refId,
             "fileName": fileName,
             "chunk": f"chunk{section}",
-            "data": data1
+            "data": data1,
         }
 
         print(f"Callback URL: {CALLBACK_URL}/g{section}")
         print(payload)
         # ยิง Callback
         async with httpx.AsyncClient() as client:
-            resp = await client.post(f"{CALLBACK_URL}/g{section}", json=payload, timeout=60.0)
+            resp = await client.post(
+                f"{CALLBACK_URL}/g{section}", json=payload, timeout=60.0
+            )
             print(f"Callback Chunk {section} Status: {resp.status_code}")
 
     except Exception as e:
         print(f"Error processing chunk {chunk_idx+1}: {e}")
 
+
 async def background_manager(body: ChunkRequest, pdf_bytes: bytes):
 
-    start_chunk_page = [v for k, v in locate_chunks(pdf_bytes=pdf_bytes, debug=False).items() if k != "last_page"]
+    start_chunk_page = [
+        v
+        for k, v in locate_chunks(pdf_bytes=pdf_bytes, debug=False).items()
+        if k != "last_page"
+    ]
     tasks = []
 
     # วนลูปสร้าง Task
-    for i in range(len(start_chunk_page)-1):
+    for i in range(len(start_chunk_page) - 1):
         start_page = start_chunk_page[i]
         end_page = start_chunk_page[i + 1]
 
@@ -223,7 +309,7 @@ async def background_manager(body: ChunkRequest, pdf_bytes: bytes):
             pdf_bytes=pdf_bytes,
             refId=body.refId,
             fileName=body.fileName,
-            start_chunk_page=start_chunk_page
+            start_chunk_page=start_chunk_page,
         )
         tasks.append(task)
 
@@ -231,35 +317,55 @@ async def background_manager(body: ChunkRequest, pdf_bytes: bytes):
     if tasks:
         await asyncio.gather(*tasks)
 
-@app.post("/api/curriculum/file")
-async def extract_curr(body: ChunkRequest, background_tasks: BackgroundTasks):
-    # โหลด PDF
+
+# @app.post("/api/curriculum/file")
+# async def extract_curr(body: ChunkRequest, background_tasks: BackgroundTasks):
+#     # โหลด PDF
+#     try:
+#         pdf_bytes = load_pdf_from_url(body.url)
+#     except Exception as e:
+#         raise HTTPException(status_code=400, detail=f"Cannot load PDF: {str(e)}")
+
+#     # ทำงาน Background
+#     background_tasks.add_task(background_manager, body, pdf_bytes)
+
+#     # ตอบกลับทันที
+#     return {"status": 200, "message": "Success. Processing started in background."}
+
+
+def background_manager_with_download(body: ChunkRequest):
     try:
         pdf_bytes = load_pdf_from_url(body.url)
+        background_manager(body, pdf_bytes)
     except Exception as e:
-        raise HTTPException(status_code=400, detail=f"Cannot load PDF: {str(e)}")
+        # log error
+        print(f"Background error: {e}")
 
-    # ทำงาน Background
-    background_tasks.add_task(background_manager, body, pdf_bytes)
+
+@app.post("/api/curriculum/file")
+async def extract_curr(body: ChunkRequest, background_tasks: BackgroundTasks):
+
+    # ส่งไป background ทั้งก้อน
+    background_tasks.add_task(background_manager_with_download, body)
 
     # ตอบกลับทันที
-    return {
-        "status": 200,
-        "message": "Success. Processing started in background."
-    }
+    return {"status": 200, "message": "Success. Processing started in background."}
+
 
 @app.get("/api/env")
 async def env():
     return {
         "OPEN_ROUTER_KEY": OPEN_ROUTER_KEY,
         "MODEL": MODEL,
-        "CALLBACK_URL": CALLBACK_URL
+        "CALLBACK_URL": CALLBACK_URL,
     }
+
 
 @app.get("/api/msg")
 async def msg():
-    return {
-        "status": 200,
-        "message": "API is running."
-    }
+    return {"status": 200, "message": "API is running."}
 
+
+@app.get("/api/msg1")
+async def msg():
+    return {"status": 200, "message": "API is running. 1"}
